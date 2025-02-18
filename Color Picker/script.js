4b91db57b499cf4c17097c3c7fbcdadeda403c4c
SureshTimma <148480722+SureshTimma@users.noreply.github.com>
2025-02-18 15:30:35 +0530

    color picker

---
+++ Color Picker/script.js
@@ -0,0 +1,18 @@
+let maindoc = document.getElementById("colorPickerContainer");
+let colorName = document.getElementById("selectedColorHexCode");
+function btn1(){
+    maindoc.style.backgroundColor = "#e0e0e0";
+    colorName.textContent ="#e0e0e0";
+}
+function btn2(){
+    maindoc.style.backgroundColor = "#6fcf97";
+    colorName.textContent ="#6fcf97";
+}
+function btn3(){
+    maindoc.style.backgroundColor = "#56ccf2";
+    colorName.textContent ="#56ccf2";
+}
+function btn4(){
+    maindoc.style.backgroundColor = "#bb6bd9";
+    colorName.textContent ="#bb6bd9";
+}
